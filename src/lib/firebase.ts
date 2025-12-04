04dd8ae7c5240a8707811cf1ed9e6153fc3aa94f
Pablo Moreno Rivera <pablomorenorivera@Pablos-MacBook-Pro.local>
2025-12-04 15:04:12 -0700

    feat: add Google Analytics

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,6 +3,7 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
+import { getAnalytics, isSupported } from 'firebase/analytics'
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -11,9 +12,13 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 }
 
 export const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 export const db = getFirestore(app)
 export const storage = getStorage(app)
+
+// Initialize analytics only in browser and if supported
+export const analytics = isSupported().then(yes => yes ? getAnalytics(app) : null)
